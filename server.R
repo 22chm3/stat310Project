4e01a2e0da74f31e842aab79bbe2844f47eef3b4
Carter Melnick <cartermelnick@Carters-MacBook-Pro-2.local>
2020-12-17 17:30:59 -0500

    Main plot complete, second plot nearly complete, everything functional

--- server.R
+++ server.R
@@ -10,7 +10,10 @@ shinyServer(function(input, output, session) {
         data <- movies %>%
             # filter(grepl(input[[countrySelect]], country, fixed=TRUE),
             #        grepl(input[[languageSelect]], language, fixed=TRUE),
-            filter(primaryGenre %in% input$genreSelect)
+            filter(primaryGenre %in% input$genreSelect,
+                   year >= input$yearSelect[1],
+                   year <= input$yearSelect[2],
+                   votes >= input$votesSelect)
         if(input$countrySelect != "All Countries") {
             data <- data %>% filter(primaryCountry == input$countrySelect)
         }
@@ -30,18 +33,15 @@ shinyServer(function(input, output, session) {
             5
         }
     })
-    # Make sure to include tabs
     # 3-4 minute video narrating how to use the app, talk about anything interesting I find
-    
-    # Allow filtering by number of votes
+
     
     # For tomorrow: first make smaller dataset with only data points with budget and revenue
     # Check if worldwide and US revenue are really different (they probably are for intl films)
     # Then make scatterplot of budget vs revenue, give some filtering options
-    # Maybe put a couple buttons like "highlight cult classics" that color points and makes an annotation
-    # pop up explaining what the button did, include "reset plot" button? Idk
     # Third tab: bar chart of most popular? Best and worst rated? Ratings over time histogram? For 
     # univariate stuff, just filter out everything that doesn't have nice values
+    # Put a "show break even line" checkbox
     
     output$genreSelect <- renderUI({
         data <- movies %>% arrange(primaryGenre)
@@ -68,17 +68,8 @@ shinyServer(function(input, output, session) {
                     selected = "English")
     })
 
-    output$moviePlot <- renderPlotly({
-        # Filter the movies to display based on inputs
-        
-        #     filter(Year == input$year)
-        # if(input$choices == "regions") {
-        #     yearData <- yearData %>% filter(Region %in% input$region)
-        # }
-        # if(input$choices == "countries") {
-        #     yearData <- yearData %>% filter(Country %in% input$country)
-        # }
-        
+    output$ratingsPlot <- renderPlotly({
+        # Generate color palette for genres
         pal <- c('#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', 
                  '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', 
                  '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#000000')
@@ -100,7 +91,9 @@ shinyServer(function(input, output, session) {
                      colors=pal,
                      alpha = 0.5,
                      name=~primaryGenre,
-                     text=~original_title,
+                     hovertext=paste0("<b>",movieData()$original_title, "</b>",
+                                      "<br><b>Release Year: </b>", movieData()$year,
+                                      "<br><b>Average User Rating: </b>", movieData()$avg_vote),
                      hoverinfo="text"
                      ) %>%
             layout(title=list(text="Movies Over the Years by Ratings"),
@@ -119,6 +112,7 @@ shinyServer(function(input, output, session) {
         if(is.null(clickData)) {
             "Click on a movie to view more information about it!"
         } else {
+            # Select the movie the user clicked on and a subset of variables
             selectedMovie <- movieData() %>%
                 filter(imdb_title_id == clickData$key) %>%
                 mutate(year = as.integer(year), 
@@ -137,6 +131,7 @@ shinyServer(function(input, output, session) {
         if(is.null(clickData)) {
             NULL
         } else {
+            # Select the movie the user clicked on and a subset of variables
             selectedMovie <- movieData() %>%
                 filter(imdb_title_id == clickData$key) %>%
                 mutate(year = as.integer(year), 
@@ -155,6 +150,7 @@ shinyServer(function(input, output, session) {
         if(is.null(clickData)) {
             NULL
         } else {
+            # Select the movie the user clicked on and a subset of variables
             selectedMovie <- movieData() %>%
                 filter(imdb_title_id == clickData$key) %>%
                 mutate(year = as.integer(year), 
@@ -164,6 +160,68 @@ shinyServer(function(input, output, session) {
                 rename(Description=description)
             selectedMovie
         }
+    })
+    
+    output$genreSelectRev <- renderUI({
+        data <- movies %>% arrange(primaryGenre)
+        selectInput("genreSelectRev", 
+                    "Select genres:",
+                    choices = c("All Genres", unique(data$primaryGenre)),
+                    selected = "All Genres",
+                    multiple=TRUE)
+    })
+    
+    output$revenuePlot <- renderPlotly({
+        data <- movies
+        newData <- data %>% drop_na(c(budgetNum, incomeNum)) %>%
+                            filter(isDollar == TRUE) %>% 
+                            mutate(incomeNum = as.numeric(incomeNum),
+                                   budgetNum = as.numeric(budgetNum))
+        if(!"All Genres" %in% input$genreSelectRev) {
+            newData <- newData %>% filter(primaryGenre %in% input$genreSelectRev)
+        }
+        # Create color palette for genres
+        pal <- c('#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', 
+                 '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', 
+                 '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#000000')
+        pal <- setNames(pal, c("Romance","Biography","Drama","Adventure","History","Crime","Western",
+                               "Fantasy","Comedy","Horror","Family","Action","Mystery","Sci-Fi",
+                               "Animation","Thriller","Musical","Music","War","Film-Noir","Sport"))
+        
+        p <- plot_ly(data=newData, 
+                     source = "revenuePlot",
+                     key = ~imdb_title_id,
+                     type="scatter",
+                     mode="markers",
+                     x=~budgetNum,
+                     y=~incomeNum,
+                     #size=~size,
+                     #sizes=5,
+                     marker=list(sizemode="diameter"),
+                     color=~primaryGenre,
+                     colors=pal,
+                     alpha = 0.5,
+                     name=~primaryGenre,
+                     hovertext=paste0("<b>",newData$original_title, "</b>",
+                                         "<br><b>Budget: </b>", newData$budget,
+                                         "<br><b>Income: </b>", newData$worlwide_gross_income),
+                     hoverinfo="text",
+                     height=700
+        ) %>%
+            layout(title=list(text="Movie Budgets vs Revenue"),
+                   xaxis=list(title="Budget", range=c(0,350000000)),
+                   yaxis=list(title="Worldwide Revenue", range=c(0,2100000000)),
+                   legend=list(x=0.5, y=-0.2, xanchor="center",
+                               orientation="h", itemsizing="constant")
+            ) %>%
+            config(displayModeBar = F)
+        if(input$breakEvenLine == TRUE) {
+            evenLine <- list(type="line", line=list(color="black"),xref="x",yref="y",
+                             x0=0, x1=350000000, y0=0, y1=350000000)
+            p <- p %>% layout(shapes = evenLine)
+        } 
+        
+        p
     })
 
 })
--- ui.R
+++ ui.R
@@ -2,24 +2,52 @@ library(shiny)
 library(plotly)
 
 shinyUI(fluidPage(
-
-    titlePanel("IMDB Movie Data"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            uiOutput("genreSelect"),
-            uiOutput("countrySelect"),
-            uiOutput("languageSelect"),
-            checkboxInput("sizeControl",
-                          "Show vote count as point size")
-        ),
-
-        mainPanel(
-            plotlyOutput("moviePlot"),
-            tableOutput("movieInfo1"),
-            tableOutput("movieInfo2"),
-            tableOutput("movieInfo3")
-            
-        )
+    tabsetPanel(
+        tabPanel("Ratings", fluid=TRUE,
+                 titlePanel("IMDB Movie Data"),
+                 
+                 sidebarLayout(
+                     sidebarPanel(
+                         uiOutput("genreSelect"),
+                         uiOutput("countrySelect"),
+                         uiOutput("languageSelect"),
+                         sliderInput("yearSelect",
+                                     "Select year range:",
+                                     min = 1894, 
+                                     max = 2020,
+                                     value = c(1894, 2020),
+                                     sep=""),
+                         sliderInput("votesSelect",
+                                     "Minimum number of user votes:",
+                                     min = 100,
+                                     max = 10000,
+                                     value = 100),
+                         checkboxInput("sizeControl",
+                                       "Show vote count as point size")
+                     ),
+                     
+                     mainPanel(
+                         plotlyOutput("ratingsPlot"),
+                         tableOutput("movieInfo1"),
+                         tableOutput("movieInfo2"),
+                         tableOutput("movieInfo3")
+                         
+                     )
+                 )),
+        tabPanel("Revenue", fluid=TRUE,
+                 titlePanel("IMDB Movie Data"),
+                 
+                 sidebarLayout(
+                     sidebarPanel(
+                         uiOutput("genreSelectRev"),
+                         checkboxInput("breakEvenLine",
+                                       "Show break-even line")
+                     ),
+                     
+                     mainPanel(
+                         plotlyOutput("revenuePlot")
+                     )
+                 ))
     )
+    
 ))
